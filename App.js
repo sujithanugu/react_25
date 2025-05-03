2bc33b57d9d51610b754e238e31d44a7f34c8841
Sujithkumar Reddy <sujithkumar.reddy@vistaar.local>
2025-05-03 16:43:28 +0530

    change

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 const heading = React.createElement("div", { id: "heading" }, [
-  React.createElement("h1", {}, "h1 tag!!"),
+  React.createElement("h1", {}, "h2 tag!!"),
   React.createElement("h2", {}, "h2 tag!!"),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
